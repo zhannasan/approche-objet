4a81650abaeafc6eb7575bc5b75ce0f213c43bc4
Zhanna <j.santybay@gmail.com>
2019-11-13 16:44:08 +0100

    6th commit : tp4

---
+++ src/main/java/utils/ConversionNombre.java
@@ -0,0 +1,15 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		String str = "4753";
+		int toInt = Integer.valueOf(str);
+		
+		int a = 558393;
+		int b = 73383;
+		int max = Integer.max(a, b);
+		System.out.println(max);
+	}
+
+}
