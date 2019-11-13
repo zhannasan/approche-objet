d3f6889860935e5c40eb16272244904ca28a93d0
Zhanna <j.santybay@gmail.com>
2019-11-13 11:10:54 +0100

    1st commit

---
+++ src/main/java/entites/AdressePostale.java
@@ -0,0 +1,10 @@
+package entites;
+
+public class AdressePostale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/essais/TestAdressePostale.java
@@ -0,0 +1,13 @@
+package essais;
+
+import entites.AdressePostale;
+
+public class TestAdressePostale {
+
+	public static void main(String[] args) {
+		AdressePostale adA = new AdressePostale();
+		adA.
+		
+		}
+
+}
